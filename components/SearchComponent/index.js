ae0793149693eff726618f048c81a0bacd739e14
sidv1905 <sidvarangaonkar1905@gmail.com>
2021-12-14 13:30:07 +0530

    first commit

---
+++ components/SearchComponent/index.js
@@ -0,0 +1,14 @@
+export default function SearchComponent() {
+  return (
+    <div>
+      <h1>Enter question</h1>
+      <input type="text" id="search" placeholder="Search..." />
+      <button type="text" id="search-button">
+        Search
+      </button>
+      <div>
+        <h3>Answer :</h3>
+      </div>
+    </div>
+  );
+}
