3e6d617684c3517ca22860ff39b83d5dccdde8de
sidv1905 <sidvarangaonkar1905@gmail.com>
2021-12-15 16:25:12 +0530

    Completed

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,7 +2,15 @@ import styles from "./Header.module.css";
 import Image from "next/image";
 import logo from "../../public/jurata.svg";
 import Link from "next/link";
+import { useState } from "react";
+import { FaHamburger } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
 export default function Header({ rightHeader }) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleDrawer = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <div className={styles.Header}>
       <div className={styles.left}>
@@ -16,6 +24,27 @@ export default function Header({ rightHeader }) {
         <h4>QA-Bank</h4>
       </div>
       <div className={styles.right}>
+        <Link href="/" locale="en">
+          {rightHeader.English}
+        </Link>
+        <Link href="/" locale="de">
+          {rightHeader.German}
+        </Link>
+      </div>
+
+      <div className={styles.rightMobile} onClick={toggleDrawer}>
+        <FaHamburger size="30" color="white" />
+      </div>
+      <div
+        className={
+          showMenu
+            ? `${styles.languageDrawer} ${styles.activateDrawer}`
+            : `${styles.languageDrawer}`
+        }
+      >
+        <div className={styles.cross}>
+          <ImCross size="30" color="white" onClick={toggleDrawer} />
+        </div>
         <Link href="/" locale="en">
           {rightHeader.English}
         </Link>
